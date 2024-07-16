69e76ebf81279eebe9e75946f87130d29641ec47
shaloojais8876 <104994730+shaloojais8876@users.noreply.github.com>
2024-07-16 14:32:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+let student = {
+	name : "shalini jaiswal"
+}
+Object.prototype.getkeys = function(){
+	// 'this' refers to the object on which the method is called
+	return Object.keys(this);
+}
+console.log(student.getkeys());
